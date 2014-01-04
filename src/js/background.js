bac098446583352213f73f7b89267001a68fa887
Catalin Dumitru <colin.dumitru@gmail.com>
2014-01-04 17:43:18 +0200

    first version, stream are shown but formatting and options needs to be changed

--- src/js/background.js
+++ src/js/background.js
@@ -1,67 +1,87 @@
-var data = {},
-	options = {};
+var CLIENT_ID = '4vuxrrhrnica3vvknwc11jdy1k07oum',
+	streams = [],
+	streamsRemaining = 0,
+	options = {
+		streams : ['twitch']
+	},
+
+	streamToBinder = {
+		'twitch' : getTwitchStreams
+	};
 
 function optionsChangedListener() {
-	chrome.storage.sync.get('apikey', function(value) {
-		options = value;
-		getUserInformation();
+	chrome.storage.sync.get('streams', function(value) {
+		//todo options = value;
+		getStreams();
 	});
 }
 
-function getUserInformation() {
+function getStreams() {
+	streams = [];
+	streamsRemaining = options.streams.length;
+
+	for (var stream_id in options.streams) {
+		streamToBinder[options.streams[stream_id]]();
+	}
+}
+
+// -------------------------- Twitch specific -------------------------------
+
+function getTwitchStreams() {
 	//todo remove dependency to jquery
 	$.ajax({
 		type: 'GET',
-		url: 'https://bitminter.com/api/users',
+		url: 'https://api.twitch.tv/kraken/streams.json?game=Hearthstone%3A%20Heroes%20of%20Warcraft',
 		headers: {
-			Authorization: 'key=' + options.apikey
+			Accept: 'application/vnd.twitchtv.v2+json'
 		},
 		success: function(newData) {
-			data = newData;
+			streams = newData.streams.map(twitchJsonToStream);
+			console.log(streams);
 			sendData();
 		},
 		error: function() {
-			data = {};
-			sendData();
+			//todo
 		}
 	});
 }
 
+function twitchJsonToStream(json) {
+	return {
+		title : json.name,
+		logo : json.preview,
+		viewers : json.viewers,
+		user : json.channel.display_name,
+		url : json._links.self
+	}
+}
+
+// ---------------------------------------------------------------------------
+
 function sendData() {
-	chrome.browserAction.setBadgeText({text: String(getHashRate(parseFloat(data.hash_rate)))});
+	/*Only update popup if all the pages have finished loading*/
+	if(--streamsRemaining) {
+		return;
+	}
+
+	chrome.browserAction.setBadgeText({text: String(streams.length)});
 
 	var views = chrome.extension.getViews();
 
 	for (var i = 0; i < views.length; i++) {
 		var view = views[i];
 
-		view.data = data;
+		view.streams = streams;
 		view.options = options;
 		view.update();
 	}
-}
-
-function popupOpened() {
-	sendData();
-}
-
-function getHashRate(rate) {
-	while(rate > 1000) {
-		rate /= 1000;
-	}
-	return Math.floor(rate);
-}
-
-function update() {
 }
 
 function run() {
 	chrome.storage.onChanged.addListener(optionsChangedListener);
 	optionsChangedListener();	
 
-	chrome.browserAction.onClicked.addListener(popupOpened);
-
-	window.setInterval(getUserInformation, 5000);
+	window.setInterval(getStreams, 5000);
 }
 
 run();
--- src/js/main.js
+++ src/js/main.js
@@ -1,136 +1,37 @@
 var	jq = chrome.extension.getBackgroundPage().$,
- 	data = chrome.extension.getBackgroundPage().data,
-	options = chrome.extension.getBackgroundPage().options,
-	worker_id = 'general',
-	worker_update_function = updateGeneralPage;
+ 	streams = chrome.extension.getBackgroundPage().streams,
+	options = chrome.extension.getBackgroundPage().options;
 
 function start() {
 	$(document).ready(onload);
 }	
 
 function update() {
-	if(!options.apikey || !options.apikey.length) {
-		$('#error', document).text('API key must be set from the options page');		
-	} else {
-		updateWorkerList();
-		updateCurrentPage();
-		updateWorkerSelection();
+	var container = $('#streams', document);
+
+	container.children().remove();
+
+	for (var i in streams) {
+		var stream = streams[i];
+
+		container.append(template('stream_template',
+			stream.title, stream.viewers, stream.user, stream.url, stream.logo));
 	}
 }
 
-function updateWorkerList() {
-	$('#workers_list', document).children().remove();
+function template(id) {
+	var script = $('#' + id, document).text();
 
-	$('#workers_list', document).append('<div id="worker_general" binder="createGeneralPage" worker_id="general">&#x2190;general</div>')
-
-	for (var i in data.workers) {
-		var worker = data.workers[i];
-		$('#workers_list', document).append('<div id="worker_' + i + '" class="worker_label" binder="createWorkerPage" worker_id="' + i +'">' + worker.name + '</div>')
+	for (var i = 1; i < arguments.length; i++) {
+		script = script.replace('$' + i, arguments[i]);
 	}
 
-	$('#workers_list', document).children().click(workerClicked);
-}
-
-function workerClicked() {
-	var moi = $(this);
-	worker_id = moi.attr('worker_id');
-
-	window[moi.attr('binder')]();
-	updateCurrentPage();
-	updateWorkerSelection();
-}
-
-function updateWorkerSelection() {
-	$('.worker_label_selected', document).removeClass('worker_label_selected');
-	$('#worker_' + worker_id, document).addClass('worker_label_selected');
-}
-
-function updateCurrentPage() {
-	$('#loading_overlay', document).hide();
-	worker_update_function();
-}
-
-//--------------------------------------------------------------------------
-
-function updateGeneralPage() {
-	var rate = convertHashRate(parseFloat(data.hash_rate));
-
-	$('#username', document).text(data.name);
-
-	//todo cache these
-	$("#hash_rate", document).text(rate.val);
-	$("#hash_rate_suffix", document).text(rate.suffix);
-
-	$("#accepted", document).text(data.shift.accepted);
-	$("#rejected", document).text(data.shift.rejected);
-	$("#user_score", document).text(parseFloat(data.shift.user_score).toFixed(10));
-	$("#total_score", document).text(parseFloat(data.shift.total_score).toFixed(10));
-
-	$("#btc", document).text(parseFloat(data.balances.BTC).toFixed(10));
-	$("#nmc", document).text(parseFloat(data.balances.NMC).toFixed(10));
-}
-
-function createGeneralPage() {
-	worker_update_function = updateGeneralPage;
-	$('#page', document).html(getTemplate('general_page'));
-}
-
-//--------------------------------------------------------------------------
-
-function updateWorkerPage() {
-	var rate = convertHashRate(parseFloat(data.workers[worker_id].hash_rate));
-
-	$('#username', document).text(data.workers[worker_id].name);
-
-	//todo cache these
-	$("#hash_rate", document).text(rate.val);
-	$("#hash_rate_suffix", document).text(rate.suffix);
-
-	$("#btc_round_accepted", document).text(data.workers[worker_id].work.BTC.round_accepted);
-	$("#btc_round_rejected", document).text(data.workers[worker_id].work.BTC.round_rejected);
-	$("#btc_total_accepted", document).text(data.workers[worker_id].work.BTC.total_accepted);
-	$("#btc_total_rejected", document).text(data.workers[worker_id].work.BTC.total_rejected);
-
-	$("#nmc_round_accepted", document).text(data.workers[worker_id].work.NMC.round_accepted);
-	$("#nmc_round_rejected", document).text(data.workers[worker_id].work.NMC.round_rejected);
-	$("#nmc_total_accepted", document).text(data.workers[worker_id].work.NMC.total_accepted);
-	$("#nmc_total_rejected", document).text(data.workers[worker_id].work.NMC.total_rejected);	
-}
-
-function createWorkerPage() {
-	worker_update_function = updateWorkerPage;
-	$('#page', document).html(getTemplate('worker_page'));
-}
-
-//--------------------------------------------------------------------------
-
-function getTemplate(id) {
-	return $("#" + id, document).html();
-}
-
-function convertHashRate(value) {
-	var suffix = 'MH/s';
-
-	if (value > 1000) {
-		value /= 1000;
-		suffix = 'GH/s';
-	}
-
-	if (value > 1000) {
-		value /= 1000;
-		suffix = 'TH/s';
-	}
-
-	return {
-		val: Math.round(value * 10) / 10,
-		suffix: suffix
-	};
+	return $('<div></div>')
+		.append(script);
 }
 
 function onload() {
-	createGeneralPage();
 	update();
-	bind();
 }
 
 function bind() {
